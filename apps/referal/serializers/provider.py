74330a07487c29a134177e777b0462419aacb03b
RadySonabu <ardynubanos@yahoo.com>
2021-06-17 12:10:23 +0800

    Renamed fields in provider serializers

--- apps/referal/serializers/provider.py
+++ apps/referal/serializers/provider.py
@@ -1,4 +1,6 @@
 from ..models import Location, Provider, Category
+from ..serializers.category import CategorySerializer
+from ..serializers.location import LocationSerializer
 from rest_framework import serializers
 from rest_framework import viewsets
 
@@ -16,8 +18,17 @@ class InputProviderSerializer(serializers.ModelSerializer):
 
 
 class OutputProviderSerializer(serializers.ModelSerializer):
+    # category = serializers.PrimaryKeyRelatedField(
+    #     queryset=Category.objects.select_related().all(), source='category_id')
+    # location = serializers.PrimaryKeyRelatedField(
+    #     queryset=Location.objects.select_related().all(), source='location_id')
+    # category = serializers.CharField(source='category_id', )
+    # location = serializers.CharField(source='location_id', )
+    category = CategorySerializer(source='category_id')
+    location = LocationSerializer(source='location_id')
 
     class Meta:
         model = Provider
-        fields = "__all__"
+        fields = ['id', 'is_active', 'modified_by', 'modified_date',
+                  'name', 'phone_number', 'category', 'location']
         depth = 2
